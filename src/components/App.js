775cfdca8a1fb2e4c6d58f4fa3c20af9982c85e6
cynthia <cynthiakagz@gmail.com>
2023-06-01 23:00:56 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,55 @@
-// create your App component here
+// // create your App component here
+// import React, { useEffect, useState } from "react";
+
+// function App() {
+//     const[dogImage, setDogImage] = useState(null);
+
+
+// useEffect(() => {
+//     fetch("https://dog.ceo/api/breeds/image/random")
+//     .then((response) => response.json())
+//     .then((dogImage) => setDogImage(dogImage))
+//     .catch((error) => console.error(error));
+// }, []);
+
+// return (
+//     <div>
+//         {dogImage ? (
+//             <img src={dogImage} alt="A random Dog"/>
+//         ) : (
+//             <p>Loading...</p>
+//         )}
+//     </div>
+
+// );
+// }
+// export default App;
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
